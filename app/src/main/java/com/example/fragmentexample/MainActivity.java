41f074e0f05bc16f0abab2857e45efbd50376f6e
Jacob <strickerrj21@hanover.edu>
2020-04-07 11:13:42 -0400

    Added new resources, optimized imports and added SimpleFragment.java

--- app/src/main/java/com/example/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample/MainActivity.java
@@ -1,11 +1,12 @@
-package com.example.android.fragmentexample;
+package com.example.fragmentexample;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
